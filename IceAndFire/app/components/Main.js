7b030c545ba71818a7c44899afa2c9dceee376d6
Arnab Kumar Shil <arnabkumarshil@Arnabs-MacBook-Pro.local>
2018-01-21 20:39:43 +0600

    Initial structure built

---
+++ IceAndFire/app/components/Main.js
@@ -0,0 +1,46 @@
+import {
+    Platform,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+import React, { Component } from 'react';
+
+export default class Main extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Welcome to React Native!
+            </Text>
+                <Text style={styles.instructions}>
+                    To get started, edit App.js
+            </Text>
+                <Text style={styles.instructions}>
+                    {instructions}
+                </Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
+
