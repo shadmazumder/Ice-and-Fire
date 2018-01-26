74a0bd527c6b32a5c3a102b78f00efac82a31fcc
SHUVRO <shuvro@SHUVROs-MacBook-Pro.local>
2018-01-26 23:49:25 +0600

    listComponent reviewed and simple change in quotation.

--- IceAndFire/app/components/ListComponent.js
+++ IceAndFire/app/components/ListComponent.js
@@ -1,59 +1,54 @@
 import React, {Component} from 'react';
 import {List, ListItem} from 'react-native-elements';
-import {
-    Text,
-    View,
-    FlatList
-} from 'react-native';
+import {Text, View, FlatList} from 'react-native';
 
+export default class ContainerList extends Component {
+  state = {
+    data: ['Item 1', 'Item 2', 'Item 3'],
+  };
 
-export default class ContainerList extends Component{
-    
-    state = {
-        data: ['Item 1', 'Item 2', 'Item 3']
-    }
+  getItemSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: '85%',
+          marginLeft: '15%',
+          backgroundColor: '#CED0CE',
+        }}
+      />
+    );
+  };
 
-    getItemSeparator = ()=> {
-        return(
-            <View
-                style = {{
-                    height: 1,
-                    width: '85%',
-                    marginLeft: `15%`,
-                    backgroundColor: `#CED0CE`
-                }}
-            />
-        );
-    };
+  render () {
+    return (
+      <View style={this.props.containerStyle}>
 
-    render(){
-        return (
-            <View style = {this.props.containerStyle}>
-            
-                <Text style = {this.props.headerStyle}>
-                    {this.props.headerText}
-                </Text>
+        <Text style={this.props.headerStyle}>
+          {this.props.headerText}
+        </Text>
 
-                <List containerStyle={{
-                                        flex: 2,
-                                        borderTopWidth: 0, 
-                                        borderBottomWidth: 0
-                                    }} >
-                    <FlatList 
-                        data = {this.state.data}
-                        renderItem = {({ item }) => (
-                            <ListItem 
-                                containerStyle = {{borderBottomWidth: 0}}
-                                title = {item}
-                                subtitle = {item}
-                            />
-                        )}
-                        // keyExtractor = {item=>item.id}
-                        ItemSeparatorComponent = {this.getItemSeparator}
-                    />
-                </List>
-            </View>
-        )
-    }
+        <List
+          containerStyle={{
+            flex: 2,
+            borderTopWidth: 0,
+            borderBottomWidth: 0,
+          }}
+        >
+          <FlatList
+            data={this.state.data}
+            renderItem={({item}) => (
+              <ListItem
+                containerStyle={{borderBottomWidth: 0}}
+                title={item}
+                subtitle={item}
+              />
+            )}
+            // keyExtractor = {item=>item.id}
+            ItemSeparatorComponent={this.getItemSeparator}
+          />
+        </List>
+      </View>
+    );
+  }
 }
-
