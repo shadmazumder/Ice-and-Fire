9447629d008b8fd2ad3d5af27de2b6b60f063c5b
SHUVRO <shuvro@SHUVROs-MacBook-Pro.local>
2018-01-28 00:36:23 +0600

    regin set in AvaterListItem

--- app/components/AvatarListItem.js
+++ app/components/AvatarListItem.js
@@ -8,7 +8,7 @@ export default class AvatarListItem extends Component {
         // roundAvatar
         containerStyle={{borderBottomWidth: 0}}
         title={this.props.item.name}
-        subtitle={`${this.props.item.publisher}, ${this.props.item.country}`}
+        subtitle={`${this.props.item.region}`}
         // avatar = {this.props.avatarUrl}
         onPress = {()=>{this.props.onPress(this.props.item)}}
       />
