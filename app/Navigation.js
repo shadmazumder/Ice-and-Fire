431d62bf714ca839a5357ffeb51b77ab3c2754aa
Shad Rayhan Mazumder <shadmazumder@gmail.com>
2018-01-27 20:48:55 +0600

    [Issues-10]
    Adding icon for tabbar.

--- app/Navigation.js
+++ app/Navigation.js
@@ -2,16 +2,24 @@ import React from 'react';
 import { StackNavigator, TabNavigator } from 'react-navigation';
 import HouseScreen from './components/HouseScreen';
 import BookScreen from './components/BookScreen';
-
+import {Icon} from 'react-native-elements'
 const BaseNavigator = TabNavigator ({
     House: {
         screen: HouseScreen,
-
+        navigationOptions:{
+            tabBarLabel: 'House',
+            tabBarIcon: ({ tintColor }) => 
+                    <Icon name="home" size={35} color={tintColor}/>,
+        },
     },
 
     Book:{
         screen: BookScreen,
-
+        navigationOptions:{
+            tabBarLabel: 'Book',
+            tabBarIcon: ({ tintColor }) => 
+                    <Icon name="book" size={35} color={tintColor}/>,
+        },
     },
 })
 
