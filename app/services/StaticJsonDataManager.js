f4d1eb1c3cd22b2b1eaa3a2760b55fc36d3e7549
Asaduzzaman Shuvro <shuvro@blockbonds.io>
2018-01-29 19:10:07 +0600

    code formatting.

--- app/services/StaticJsonDataManager.js
+++ app/services/StaticJsonDataManager.js
@@ -1,13 +1,12 @@
 import jsonData from '../resources/data.json'
 import React, { Component } from 'react';
 
-export default class StataticJsonDataManager{
-    getHouses(){
-        console.log(jsonData.houses)
+export default class StataticJsonDataManager {
+    getHouses() {
         return jsonData.houses;
     }
 
-    getBooks(){
-        return jsonData.books
+    getBooks() {
+        return jsonData.books;
     }
 }
